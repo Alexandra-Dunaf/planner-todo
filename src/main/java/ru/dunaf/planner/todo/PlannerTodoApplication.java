cc2c50a3f6beb0edb18aee5a71c74a5a084e788e
Alexandra_Dunaf <sasha-kalugina@yandex.ru>
2023-10-08 21:29:36 +0400

    commented out the legacy code MS interaction via AMQP

--- src/main/java/ru/dunaf/planner/todo/PlannerTodoApplication.java
+++ src/main/java/ru/dunaf/planner/todo/PlannerTodoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"ru.dunaf.planner"})
 @EnableJpaRepositories(basePackages = {"ru.dunaf.planner.todo.repo"})
 @EntityScan(basePackages = {"ru.dunaf.planner.entity"})
+@RefreshScope
 public class PlannerTodoApplication {
 
     public static void main(String[] args) {
